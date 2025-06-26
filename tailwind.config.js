395a51c17943a63dc92ba8bde1fb2bdfd00b4dde
eder.ramosh <eder.ramosh@gmail.com>
2025-06-26 10:52:19 -0600

    Setting new project of LoopStudios

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Josefin Sans", "sans-serif"],
+        alata: ["Alata", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+  future: {
+    disableExperimentalFeatures: true,
+  },
+};
+
+/*
+comando para crear el watcher   
+npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
+*/
